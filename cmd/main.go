41bac7c9e6fe36281b5c038c0c239bf33bb0aa82
Nesanesis <orionvega2343@gmail.com>
2026-04-28 18:36:26 +0300

    feat: add main router and server

--- cmd/main.go
+++ cmd/main.go
@@ -3,15 +3,59 @@ package main
 import (
 	"LibrariAPI/internal/config"
 	"LibrariAPI/internal/db"
+	"LibrariAPI/internal/handler"
+	"LibrariAPI/internal/repository"
+	"LibrariAPI/internal/service"
 	"fmt"
+	"net/http"
+
+	mw "LibrariAPI/internal/middleware"
+
+	"github.com/go-chi/chi/v5"
 )
 
 func main() {
 	cfg := config.MustLoad()
-	db, err := db.Connect(cfg)
+	datb, err := db.Connect(cfg)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println("db connected", db)
+	userRepo := repository.NewUserRepository(datb)
+	userService := service.NewUserService(userRepo, cfg.JWT.Secret)
+	userHandler := handler.NewHandler(userService)
 
+	authorRepo := repository.NewAuthorRepository(datb)
+	authorService := service.NewAuthorService(authorRepo)
+	authorHandler := handler.NewAuthorHandler(authorService)
+
+	bookRepo := repository.NewBookRepository(datb)
+	bookService := service.NewBookService(bookRepo)
+	bookHandler := handler.NewBookHandler(bookService)
+
+	reviewsRepo := repository.NewReviewRepository(datb)
+	reviewsService := service.NewReviewService(reviewsRepo)
+	reviewsHandler := handler.NewReviewHandler(reviewsService)
+
+	r := chi.NewRouter()
+	r.Use(mw.LoggerMiddleware)
+	r.Post("/auth/register", userHandler.CreateUser)
+	r.Post("/auth/login", userHandler.Login)
+
+	r.Group(func(r chi.Router) {
+		r.Use(func(next http.Handler) http.Handler {
+			return mw.AuthMiddleware(next, cfg.JWT.Secret)
+		})
+		r.Get("/authors/{id}", authorHandler.GetAuthorById)
+		r.Post("/authors", authorHandler.AddAuthor)
+		r.Get("/books/{id}", bookHandler.GetBookById)
+		r.Post("/books", bookHandler.CreateBook)
+		r.Get("/reviews/{id}", reviewsHandler.GetReviewsById)
+		r.Post("/reviews", reviewsHandler.CreateReview)
+
+	})
+	fmt.Println("Сервер запщуен на порту 8080")
+	err = http.ListenAndServe(":8080", r)
+	if err != nil {
+		panic(err)
+	}
 }
--- internal/handler/user_handler.go
+++ internal/handler/user_handler.go
@@ -35,3 +35,19 @@ func (h *Handler) CreateUser(w http.ResponseWriter, r *http.Request) {
 	}
 
 }
+func (h *Handler) Login(w http.ResponseWriter, r *http.Request) {
+	var user model.User
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	token, err := h.s.Login(&user, user.Email)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusUnauthorized)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(map[string]string{"token": token})
+}
